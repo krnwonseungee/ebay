d1a1451bda9eb38ebd7d96f9dd6bb53f28f0bd2b
John Nguyen <jdnguyenxo@gmail.com>
2014-04-21 16:28:01 -0400

    Update payment_details.rb

--- lib/ebay/types/payment_details.rb
+++ lib/ebay/types/payment_details.rb
@@ -11,7 +11,7 @@ module Ebay # :nodoc:
       root_element_name 'PaymentDetails'
       numeric_node :hours_to_deposit, 'HoursToDeposit', :optional => true
       numeric_node :days_to_full_payment, 'DaysToFullPayment', :optional => true
-      # money_node :deposit_amount, 'DepositAmount', :optional => true
+      money_node :deposit_amount, 'DepositAmount', :optional => true
       text_node :deposit_type, 'DepositType', :optional => true
     end
   end
