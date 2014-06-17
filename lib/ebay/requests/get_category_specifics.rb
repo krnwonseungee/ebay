7b392ab63cf2897d3ffafa4740f6b160732cdeef
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-06-17 15:32:03 -0700

    Adds sample get_category_specifics request

--- lib/ebay/requests/get_category_specifics.rb
+++ lib/ebay/requests/get_category_specifics.rb
@@ -30,3 +30,23 @@ module Ebay # :nodoc:
 end
 
 
+
+item = Ebay::Requests::GetCategorySpecifics.new(
+  :category_ids => Ebay::Requests::GetCategorySpecifics.new( category_ids: [53159] ) #sample ID. replace with item ID after add_item call
+)
+
+begin
+  # Get the item as passed in as the first argument
+  response = ebay.get_category_specifics( :category_ids => item.category_ids.category_ids )
+
+  # Get the item, which is of the type Ebay::Types::Item
+  item = response.item
+  puts "The item number is: #{item.item_id}"
+  puts "The item was listed at: #{item.listing_details.start_time}"
+  puts "The item can be viewed at: #{item.listing_details.view_item_url}"
+  puts "The starting price is: #{item.listing_details.converted_start_price.format(:with_currency)}"
+rescue Ebay::RequestError => e
+  e.errors.each do |error|
+    puts error.long_message
+  end
+end
