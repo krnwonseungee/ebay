8d094fa19f4b894974e930accc722b557ad29af3
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-06-17 15:30:57 -0700

    Adds sample get_category_features request

--- lib/ebay/requests/get_category_features.rb
+++ lib/ebay/requests/get_category_features.rb
@@ -21,3 +21,22 @@ module Ebay # :nodoc:
 end
 
 
+item = Ebay::Requests::GetCategoryFeatures.new(
+  :category_id => Ebay::Requests::GetCategoryFeatures.new( category_id: 51581 ) #sample ID. replace with item ID after add_item call
+)
+
+begin
+  # Get the item as passed in as the first argument
+  response = ebay.get_category_features( :category_id => item.category_id.category_id )
+
+  # Get the item, which is of the type Ebay::Types::Item
+  item = response.item
+  puts "The item number is: #{item.item_id}"
+  puts "The item was listed at: #{item.listing_details.start_time}"
+  puts "The item can be viewed at: #{item.listing_details.view_item_url}"
+  puts "The starting price is: #{item.listing_details.converted_start_price.format(:with_currency)}"
+rescue Ebay::RequestError => e
+  e.errors.each do |error|
+    puts error.long_message
+  end
+end
